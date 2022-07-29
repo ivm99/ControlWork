62d8458bf0db8cae5be8b6c0078770d61f32846b
Vadim <ivm99@mail.ru>
2022-07-29 16:50:29 +0300

    String array input and print methods added

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] NewStringArrayInput(int size)
+{
+    string [] newStringArray = new string [size];
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"Введите {i}-й элемент массива: ");
+        newStringArray[i] = Console.ReadLine();
+        //Console.WriteLine();
+    }
+
+    return newStringArray;
+}
+
+void PrintArray (string [] arrayToPrint)
+{
+    Console.Write("[ ");
+
+    for (int i = 0; i < arrayToPrint.Length-1; i++)
+    {
+        Console.Write (arrayToPrint[i] + " ; ");
+    }
+
+    Console.Write (arrayToPrint[arrayToPrint.Length-1] + " ]");
+    Console.WriteLine();
+}
+
+Console.Write("Введите число элементов массива:");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string [] newStringArray = NewStringArrayInput(size);
+
+Console.Write("Введенный массив: ");
+PrintArray(newStringArray);
+
+
